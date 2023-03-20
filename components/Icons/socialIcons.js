ded762bd6092fc7c561228775351bded35a4345c
Michael Solomon <michicko81@gmail.com>
2023-03-20 09:11:33 +0100

    fixed btn and social icons

--- components/Icons/socialIcons.js
+++ components/Icons/socialIcons.js
@@ -1,5 +1,56 @@
-const SocialIcons = () => {
-  return <h1>icons</h1>;
+import styles from "./SocialIcons.module.css";
+
+import {
+  AiOutlineInstagram,
+  AiFillFacebook,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import generateUniqueId from "@/utils/generateUniqueId";
+
+const SocialIcons = ({ color }) => {
+  const links = [
+    {
+      link: "instagram.com",
+      icon: (
+        <AiOutlineInstagram
+          className={`${styles["social-icon"]} ${styles[color]}`}
+        />
+      ),
+    },
+    {
+      link: "facebook.com",
+      icon: (
+        <AiFillFacebook
+          className={`${styles["social-icon"]} ${styles[color]}`}
+        />
+      ),
+    },
+    {
+      link: "twitter.com",
+      icon: (
+        <AiOutlineTwitter
+          className={`${styles["social-icon"]} ${styles[color]}`}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <>
+      {links.map((el) => {
+        return (
+          <a
+            href={el.link}
+            key={generateUniqueId()}
+            target="_blank"
+            className={styles["social-icon-link"]}
+          >
+            {el.icon}
+          </a>
+        );
+      })}
+    </>
+  );
 };
 
 export default SocialIcons;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-import Button from "@/components/Buttons/Button";
-import RoundButton from "@/components/Buttons/RoundButton";
+import styles from "../styles/Home.module.css";
+import SocialIcons from "@/components/Icons/SocialIcons";
 import Header from "@/components/Pages/Home/Header";
-import { MdOutlineArrowRightAlt } from "react-icons/md";
+import RoundButton from "@/components/Buttons/RoundButton";
+import { ImDiamonds } from "react-icons/im";
 
 export default function Home() {
   return (
     <>
-      <Header>
-        <h1>Hello world!</h1>
-      </Header>
+      <header className={`${styles.header} row`}>
+        <div className={styles["header-bg"]}></div>
+        <div className={styles["header-logo-box"]}>
+          <ImDiamonds className={styles["header-logo-icon"]} />
+          <h1 className={styles["header-logo"]}>Magnolia</h1>
+          <ImDiamonds className={styles["header-logo-icon"]} />
+        </div>
+
+        <div className={styles["header-icons"]}>
+          <SocialIcons color={"light"} />
+        </div>
+      </header>
+      <RoundButton type={"page-direction"} />
     </>
   );
 }
